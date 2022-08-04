0884a654ba08b07684d5cc0ad47464afbe8cec7c
cadolphe <christopher.adolphe@gmail.com>
2022-08-04 08:15:45 +0400

    Added font assets and sass config-map for base styles

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,6 +6,10 @@ const Home = () => {
       <TheHero />
       
       <h2>Home Component</h2>
+
+      <div className="bg-dark-color-2" style={ { width: '400px', height: '400px', border: '1px solid #000000' } }>
+        <p className="text-align-center | text-neutral-color-2">This is some dummy content</p>
+      </div>
     </main>
   );
 };
