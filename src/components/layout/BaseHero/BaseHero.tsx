dd2687ee33f0a5866b8b77d97bdaac1ccdf8ab55
cadolphe <christopher.adolphe@gmail.com>
2022-09-10 23:53:11 +0400

    WIP: Added styles for hero section on About page

--- src/components/layout/BaseHero/BaseHero.tsx
+++ src/components/layout/BaseHero/BaseHero.tsx
@@ -1,26 +1,62 @@
+import { useRef, useCallback } from 'react';
+
+import { useMediaQuery } from '../../../hooks';
+
 import styles from './BaseHero.module.scss';
 
 interface IBaseHeroProps {
   title: string;
   description: string;
-  imgPath: string;
-  imgAltText: string;
+  bgImages: string[];
 }
 
-const BaseHero = ({ title, description, imgPath, imgAltText }: IBaseHeroProps) => {
+const BaseHero = ({ title, description, bgImages }: IBaseHeroProps) => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const handleMobileHeroImage = useCallback((mediaQueryList: MediaQueryList) => {
+    if (!divRef.current) {
+      return;
+    }
+
+    if (mediaQueryList.matches) {
+      divRef.current.style.backgroundImage = `url(${bgImages[0]})`;
+    }
+  }, [bgImages]);
+
+  const handleTabletHeroImage = useCallback((mediaQueryList: MediaQueryList) => {
+    if (!divRef.current) {
+      return;
+    }
+
+    if (mediaQueryList.matches) {
+      divRef.current.style.backgroundImage = `url(${bgImages[1]})`;
+    }
+  }, [bgImages]);
+
+  const handleDesktopHeroImage = useCallback((mediaQueryList: MediaQueryList) => {
+    if (!divRef.current) {
+      return;
+    }
+
+    if (mediaQueryList.matches) {
+      divRef.current.style.backgroundImage = `url(${bgImages[2]})`;
+    }
+  }, [bgImages]);
+
+  useMediaQuery('(min-width: 0px) and (max-width: 767px)', handleMobileHeroImage);
+  useMediaQuery('(min-width: 768px) and (max-width: 1023px)', handleTabletHeroImage);
+  useMediaQuery('(min-width: 1024px)', handleDesktopHeroImage);
+  
   return (
-    <section>
-      <div className={ `container ${styles['base-hero']}` }>
-        <div>
-          <picture>
-            <img src="" alt="" />
-          </picture>
-        </div>
+    <section className={ styles['base-hero'] }>
+      <div className="container">
+        <div className={ styles['base-hero__content'] } ref={ divRef }>
+          <div className="grid-cols">
+            <div className="grid__item grid__item--span-md-8 grid__item--span-lg-5">
+              <h1>{ title }</h1>
 
-        <div className={ styles['base-hero__content'] }>
-          <h1>{ title }</h1>
-
-          <p>{ description }</p>
+              <p>{ description }</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/views/AboutUs/AboutUs.tsx
+++ src/views/AboutUs/AboutUs.tsx
@@ -1,14 +1,17 @@
 import { BaseHero } from '../../components/layout';
 import { OurCommitment, OurQuality, OurHeadQuarters } from './index';
 
+import mobileHeroImage from '../../assets/images/about/mobile/image-hero-whitecup.jpg';
+import tabletHeroImage from '../../assets/images/about/tablet/image-hero-whitecup.jpg';
+import desktopHeroImage from '../../assets/images/about/desktop/image-hero-whitecup.jpg';
+
 const AboutUs = () => {
   return (
     <main>
       <BaseHero
         title="About Us"
-        description="Some text for the description"
-        imgPath="some image path"
-        imgAltText="The story of Coffeeroasters"
+        description="Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment."
+        bgImages={ [mobileHeroImage, tabletHeroImage, desktopHeroImage] }
       />
 
       <OurCommitment />
