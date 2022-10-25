38245240b96464b3ba9eb12e9c2562df612bc82a
cadolphe <christopher.adolphe@gmail.com>
2022-10-25 12:42:34 +0400

    WIP: Added first test for TheHero component

--- src/components/layout/TheHero/TheHero.test.tsx
+++ src/components/layout/TheHero/TheHero.test.tsx
@@ -1,13 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import TheHero from './TheHero';
 
 import { contentMap } from '../../../fixtures/contents';
+import { IContent } from '../../../interfaces/content-interface';
 
-test('should render the Hero component', () => {
-  const data = contentMap.get('home')![0];
+describe('TheHero component', () => {
+  let data: IContent;
 
-  render(<TheHero content={ data } />);
+  beforeEach(() => {
+    data = contentMap.get('home')![0];
+  });
 
-  expect(screen.getByRole('heading')).toHaveTextContent(data.title);
+
+  it('should render a title', () => {  
+    render(<TheHero content={ data } />, { wrapper: BrowserRouter });
+  
+    expect(screen.getByRole('heading')).toHaveTextContent(data.title);
+  });
 });
