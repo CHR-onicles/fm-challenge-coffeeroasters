cfc12ce8248d16b4cad13d2ac83186c13073608c
cadolphe <christopher.adolphe@gmail.com>
2022-11-17 17:01:55 +0400

    WIP: Added E2E for Create Plan page for scenario quantity and deliveries options change

--- cypress/e2e/create-your-plan.cy.ts
+++ cypress/e2e/create-your-plan.cy.ts
@@ -282,6 +282,24 @@ describe('Create Plan page', () => {
         cy.findByRole('button', { name: /Close modal/i })
           .should('be.visible');
 
+        cy.findByText(/“I drink my coffee as , with a type of bean\. ground ala , sent to me \.”/i)
+          .within(() => {
+            cy.findByText(/Filter/i)
+              .should('be.visible');
+
+            cy.findByText(/Blended/i)
+              .should('be.visible');
+
+            cy.findByText(/250g/i)
+              .should('be.visible');
+
+            cy.findByText(/Cafetière/i)
+              .should('be.visible');
+
+            cy.findByText(/Every 2 weeks/i)
+              .should('be.visible');
+          });
+
         cy.findByText(/Is this correct\? you can proceed to checkout or go back to plan selection if something is off\. subscription discount codes can also be redeemed at the checkout\./i)
           .should('be.visible');
 
@@ -314,5 +332,486 @@ describe('Create Plan page', () => {
 
     cy.findByTestId('modal-backdrop')
       .should('not.exist');
+
+    // Should open Order Summary dialog and checkout order
+    cy.findByRole('button', { name: /Create my plan/i })
+      .click();
+
+    cy.findByRole('dialog')
+      .within(() => {
+        cy.findByRole('button', { name: /Checkout/i })
+          .click();
+      });
+
+    cy.findByTestId('modal-backdrop')
+      .should('not.exist');
   });
+
+  it('should create an order with no grind option', () => {
+    // Select an option for Preferences
+    cy.findByRole('button', { name: /Preferences/i })
+      .scrollIntoView();
+
+    cy.findByRole('region', {
+      name: /How do you drink your coffee\?/i
+    }).within(() => {  
+      cy.findByText(/Capsule/i)
+        .click();
+    });
+
+    // Select an option for Bean Type
+    cy.findByRole('button', { name: /Bean Type/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /What type of coffee\?/i
+    }).within(() => {  
+      cy.findByText(/Single Origin/i)
+        .click();
+    });
+
+    // Select an option for Quantity
+    cy.findByRole('button', { name: /Quantity/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /How much would you like\?/i
+    }).within(() => {
+      cy.findByText(/500g/i)
+        .click();
+    });
+
+    // Select an option for Grind Option
+    cy.findByRole('button', { name: /Grind Option/i })
+      .should('be.disabled');
+
+    // Select an option for Deliveries
+    cy.findByRole('button', { name: /Deliveries/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /How often should we deliver\?/i
+    }).within(() => {
+      cy.findByText(/Every week/i)
+        .click();
+    });
+
+    // Should see a Order Summary with selected options
+    cy.findByRole('heading', { name: /Order Summary/i })
+      .should('be.visible');
+    cy.findByText(/“I drink my coffee using , with a type of bean\. , sent to me \.”/i)
+      .within(() => {
+        cy.findByText(/Capsule/i)
+          .should('be.visible');
+
+        cy.findByText(/Single Origin/i)
+          .should('be.visible');
+
+        cy.findByText(/500g/i)
+          .should('be.visible');
+
+        cy.findByText(/Every week/i)
+          .should('be.visible');
+      });
+
+    // Should open Order Summary dialog and checkout order
+    cy.findByRole('button', { name: /Create my plan/i })
+      .click();
+
+    cy.findByRole('dialog')
+      .within(() => {
+        cy.findByRole('heading', { name: /Order Summary/i })
+          .should('be.visible');
+
+        cy.findByRole('button', { name: /Close modal/i })
+          .should('be.visible');
+
+        cy.findByText(/“I drink my coffee using , with a type of bean\. , sent to me \.”/i)
+          .within(() => {
+            cy.findByText(/Capsule/i)
+              .should('be.visible');
+
+            cy.findByText(/Single Origin/i)
+              .should('be.visible');
+
+            cy.findByText(/500g/i)
+              .should('be.visible');
+
+            cy.findByText(/Every week/i)
+              .should('be.visible');
+          });
+
+        cy.findByText(/Is this correct\? you can proceed to checkout or go back to plan selection if something is off\. subscription discount codes can also be redeemed at the checkout\./i)
+          .should('be.visible');
+
+        cy.findByRole('heading', { name: /\$52\.00\/mo/i })
+          .should('be.visible');
+
+        cy.findByRole('button', { name: /Checkout/i })
+          .should('be.visible')
+          .click();
+      });
+
+    cy.findByTestId('modal-backdrop')
+      .should('not.exist');
+  });
+
+  it('should get appropriate order cost for the Quantity and Deliveries options selected', () => {
+    // Select an option for Preferences
+    cy.findByRole('button', { name: /Preferences/i })
+      .scrollIntoView();
+
+    cy.findByRole('region', {
+      name: /How do you drink your coffee\?/i
+    }).within(() => {  
+      cy.findByText(/Espresso/)
+        .click();
+    });
+
+    // Select an option for Bean Type
+    cy.findByRole('button', { name: /Bean Type/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /What type of coffee\?/i
+    }).within(() => {  
+      cy.findByText(/Single Origin/i)
+        .click();
+    });
+
+    // Select an option for Quantity (250g)
+    cy.findByRole('button', { name: /Quantity/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /How much would you like\?/i
+    }).within(() => {
+      cy.findByText(/250g/i)
+        .click();
+    });
+
+    // Select an option for Grind Option
+    cy.findByRole('button', { name: /Grind Option/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /Want us to grind them\?/i
+    }).within(() => {
+      cy.findByText(/Wholebean/i)
+        .click();
+    });
+
+    // Select an option for Deliveries
+    cy.findByRole('button', { name: /Deliveries/i })
+      .click();
+
+    cy.findByRole('region', {
+      name: /How often should we deliver\?/i
+    }).within(() => {
+      cy.findByText(/Every week/i)
+        .click();
+
+      cy.findByText(/\$7\.20 per shipment\. includes free first\-class shipping\./i)
+        .should('be.visible');
+
+      cy.findByText(/\$9\.60 per shipment\. includes free priority shipping\./i)
+        .should('be.visible');
+
+      cy.findByText(/\$12\.00 per shipment\. includes free priority shipping\./i)
+        .should('be.visible');
+    });
+
+    // Should see a Order Summary with selected options
+    cy.findByRole('heading', { name: /Order Summary/i })
+      .should('be.visible');
+    cy.findByText(/“I drink my coffee as , with a type of bean\. ground ala , sent to me \.”/i)
+      .within(() => {
+        cy.findByText(/Espresso/)
+          .should('be.visible');
+
+        cy.findByText(/Single Origin/i)
+          .should('be.visible');
+
+        cy.findByText(/250g/i)
+          .should('be.visible');
+
+        cy.findByText(/Wholebean/i)
+          .should('be.visible');
+
+        cy.findByText(/Every week/i)
+          .should('be.visible');
+      });
+
+    // Should show corresponding cost in Order Summary dialog
+    cy.findByRole('button', { name: /Create my plan/i })
+      .click();
+
+    cy.findByRole('dialog')
+      .within(() => {
+        cy.findByRole('heading', { name: /\$28\.80\/mo/i })
+          .should('be.visible');
+
+        cy.findByRole('button', { name: /Close modal/i })
+          .click();
+      });
+
+    // Select a new option for Deliveries
+    cy.findByRole('region', {
+      name: /How often should we deliver\?/i
+    }).within(() => {
+      cy.findByText(/Every 2 weeks/i)
+        .click();
+    });
+
+    // Should show corresponding cost in Order Summary dialog
+    cy.findByRole('button', { name: /Create my plan/i })
+      .click();
+
+    cy.findByRole('dialog')
+      .within(() => {
+        cy.findByRole('heading', { name: /\$19\.20\/mo/i })
+          .should('be.visible');
+
+        cy.findByRole('button', { name: /Close modal/i })
+          .click();
+      });
+
+  // Select a new option for Deliveries
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every month/i)
+      .click();
+  });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$12\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Close modal/i })
+        .click();
+    });
+
+  // Select a new option for Quantity (500g)
+  cy.findByRole('button', { name: /Quantity/i })
+  .click();
+
+  cy.findByRole('region', {
+    name: /How much would you like\?/i
+  }).within(() => {
+    cy.findByText(/500g/i)
+      .click();
+  });
+
+  // Select an option for Deliveries
+  cy.findByRole('button', { name: /Deliveries/i })
+    .click();
+
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every week/i)
+      .click();
+
+    cy.findByText(/\$13\.00 per shipment\. includes free first\-class shipping\./i)
+      .should('be.visible');
+
+    cy.findByText(/\$17\.50 per shipment\. includes free priority shipping\./i)
+      .should('be.visible');
+
+    cy.findByText(/\$22\.00 per shipment\. includes free priority shipping\./i)
+      .should('be.visible');
+  });
+
+  // Should see a Order Summary with selected options
+  cy.findByRole('heading', { name: /Order Summary/i })
+    .should('be.visible');
+  cy.findByText(/“I drink my coffee as , with a type of bean\. ground ala , sent to me \.”/i)
+    .within(() => {
+      cy.findByText(/Espresso/)
+        .should('be.visible');
+
+      cy.findByText(/Single Origin/i)
+        .should('be.visible');
+
+      cy.findByText(/500g/i)
+        .should('be.visible');
+
+      cy.findByText(/Wholebean/i)
+        .should('be.visible');
+
+      cy.findByText(/Every week/i)
+        .should('be.visible');
+    });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$52\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Close modal/i })
+        .click();
+    });
+
+  // Select a new option for Deliveries
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every 2 weeks/i)
+      .click();
+  });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$35\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Close modal/i })
+        .click();
+    });
+    
+  // Select a new option for Deliveries
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every month/i)
+      .click();
+  });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$22\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Close modal/i })
+        .click();
+    });
+
+  // Select a new option for Quantity (1000g)
+  cy.findByRole('button', { name: /Quantity/i })
+  .click();
+
+  cy.findByRole('region', {
+    name: /How much would you like\?/i
+  }).within(() => {
+    cy.findByText(/1000g/i)
+      .click();
+  });
+
+  // Select an option for Deliveries
+  cy.findByRole('button', { name: /Deliveries/i })
+    .click();
+
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every week/i)
+      .click();
+
+    cy.findByText(/\$22\.00 per shipment\. includes free first\-class shipping\./i)
+      .should('be.visible');
+
+    cy.findByText(/\$32\.00 per shipment\. includes free priority shipping\./i)
+      .should('be.visible');
+
+    cy.findByText(/\$42\.00 per shipment\. includes free priority shipping\./i)
+      .should('be.visible');
+  });
+
+  // Should see a Order Summary with selected options
+  cy.findByRole('heading', { name: /Order Summary/i })
+    .should('be.visible');
+  cy.findByText(/“I drink my coffee as , with a type of bean\. ground ala , sent to me \.”/i)
+    .within(() => {
+      cy.findByText(/Espresso/)
+        .should('be.visible');
+
+      cy.findByText(/Single Origin/i)
+        .should('be.visible');
+
+      cy.findByText(/1000g/i)
+        .should('be.visible');
+
+      cy.findByText(/Wholebean/i)
+        .should('be.visible');
+
+      cy.findByText(/Every week/i)
+        .should('be.visible');
+    });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$88\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Close modal/i })
+        .click();
+    });
+
+  // Select a new option for Deliveries
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every 2 weeks/i)
+      .click();
+  });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$64\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Close modal/i })
+        .click();
+    });
+    
+  // Select a new option for Deliveries
+  cy.findByRole('region', {
+    name: /How often should we deliver\?/i
+  }).within(() => {
+    cy.findByText(/Every month/i)
+      .click();
+  });
+
+  // Should show corresponding cost in Order Summary dialog
+  cy.findByRole('button', { name: /Create my plan/i })
+    .click();
+
+  cy.findByRole('dialog')
+    .within(() => {
+      cy.findByRole('heading', { name: /\$42\.00\/mo/i })
+        .should('be.visible');
+
+      cy.findByRole('button', { name: /Checkout/i })
+        .should('be.visible')
+        .click();
+    });
+  });
+
+  it('should show error messages when no options are selected', () => {});
 });
